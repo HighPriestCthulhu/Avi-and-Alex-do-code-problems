206278adf155318bb9bbc0f66160f8486691b0e1
HighPriestCthulhu <15963419+HighPriestCthulhu@users.noreply.github.com>
2021-10-16 11:52:39 +1100

    .

--- goatrope/Goatrope.py
+++ goatrope/Goatrope.py
@@ -1,10 +1,20 @@
+import sys
+
+ab=[]
+
+def main():
+    ab=[]
+    print("i")
+    for i in sys.stdin:
+        ab = i.split()
+        print(ab)
+    closest_corner(ab)
+
+def closest_corner(coords):
+    print("running")
+    xf,yf=coords[0,1]
+    print(xf,yf)
+
 if __name__ == '__main__':
     main()
 
-def main():
-    for i in sys.stdin:
-    ab = i.split()
-    print(ab)
-
-def closest_corner(xf,yf):
-    pass
